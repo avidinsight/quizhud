86fd5e8f2011169a85653929f1bdee70e170f1b4
pedro.mcmillan <pedro.mcmillan@1309e94f-fa54-0410-885a-79c1cd34a165>
2008-08-21 21:34:00 +0000

    Corrected demo videos link on docs webpage

--- website/docs.php
+++ website/docs.php
@@ -14,7 +14,7 @@ require_once('_page_header.php');
 <?php start_single_column('quizHUD Documentation'); ?>
 <p>
 As with many open source projects, documentation tends to be the last thing to get done. We'll get round to this soon!
-The quizHUD should hopefully be fairly user friendly though, and our <a href="demo" title="Click here to view our demo videos">Demo Videos</a> should help you with most areas.
+The quizHUD should hopefully be fairly user friendly though, and our <a href="demo.php" title="Click here to view our demo videos">Demo Videos</a> should help you with most areas.
 </p>
 <?php end_single_column(); ?>
 
